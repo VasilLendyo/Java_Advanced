daf580ec0663544b9297db162a9e093f413dfb51
LogosItAcademy <Tester99037@gmail.com>
2020-07-24 10:55:54 +0300

    super functionality commit#2

--- less-11/src/main/java/dao/BucketDao.java
+++ less-11/src/main/java/dao/BucketDao.java
@@ -3,7 +3,7 @@ package dao;
 import domain.Bucket;
 import shared.AbstractCRUD;
 
-//super funtionality #1001
+//super funtionality #1001 , edit #2
 
 public interface BucketDao extends AbstractCRUD<Bucket>{
 
