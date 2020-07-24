c1ce54e965c2164812644430459187f973f5851d
LogosItAcademy <Tester99037@gmail.com>
2020-07-24 09:35:35 +0300

    Revert "added comment to out origin repository"
    This reverts commit b5e1156bfbb9b613c168af968ef76c69c9a35ef3.

--- less-11/src/main/java/dao/impl/BucketDaoImpl.java
+++ less-11/src/main/java/dao/impl/BucketDaoImpl.java
@@ -13,9 +13,6 @@ import dao.BucketDao;
 import domain.Bucket;
 import utils.ConnectionUtils;
 
-
-
-//This class is used for storing Buckets modes to dataBase
 public class BucketDaoImpl implements BucketDao {
 
 	private static String CREATE = "insert into bucket(user_id, product_id, purchase_date) values (?,?,?)";
